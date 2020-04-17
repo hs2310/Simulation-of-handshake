6b10429116f792708c97b04c94e317f3fa258af3
hs231097 <harshilshah186@gmail.com>
2020-04-16 22:52:40 -0700

    Company Profile Bug Fix

--- frontend/src/component/company-side/Profile/CContactInfo/CContactInfo.js
+++ frontend/src/component/company-side/Profile/CContactInfo/CContactInfo.js
@@ -66,17 +66,17 @@ class CContactInfo extends React.Component {
             contactInfo = <div>
                 <form onSubmit={this.updateInfo}>
                     <div className="form-group">
-                        <input type="tel" name="mob" placeholder="Enter your mob" className="form-control" defaultValue={this.state.mob} onChange={this.educationChangeHandler} />
+                        <input type="tel" name="mob" placeholder="Enter your mob" className="form-control" defaultValue={this.props.mob} onChange={this.educationChangeHandler} />
                     </div>
                     <div className="form-group">
-                        <input type="email" name="email" placeholder="Enter your Email" className="form-control" defaultValue={this.state.email} onChange={this.educationChangeHandler} />
+                        <input type="email" name="email" placeholder="Enter your Email" className="form-control" defaultValue={this.props.email} onChange={this.educationChangeHandler} />
                     </div>
                     <button type="submit" className="btn btn-primary">Update</button>
                 </form>
             </div>
         }
         else {
-            contactInfo = <div>{this.state.mob}<br/>{this.state.email}</div>;
+            contactInfo = <div>{this.props.mob}<br/>{this.props.email}</div>;
         }
         return <div>
             <button onClick={this.contactInfoHandler} className="btn btn-primary" style={{ float: "right" }} type="button">edit</button>
